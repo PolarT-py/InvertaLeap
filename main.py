90771a059ff312b4460e6e2d3093993a6956bdc0
PolarT-py <147011322+PolarT-py@users.noreply.github.com>
2024-03-01 20:41:40 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+from settings import *
+
+
+class Game:
+    def __init__(self):
+        self.screen = screen
+
+    def draw(self):
+        pass
+
+    def update(self):
+        pass
+
+    def run(self):
+        pass
+
+if __name__ == "__main__":
+    game = Game()
+    game.run()
